647765ae686d61dbb6a0fbf0b33c36623867564f
Carl148Carl148 <Carl148Carl148@users.noreply.github.com>
2022-05-14 14:28:29 -0700

    Created project

---
+++ RestAPIFramework/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RestAPIFramework
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
